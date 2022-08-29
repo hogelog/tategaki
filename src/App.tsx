f2522c79d130f866622e6e1391daa766ef3b447a
hogelog <konbu.komuro@gmail.com>
2022-08-29 23:33:06 +0900

    Implement 20x20 graph paper

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,48 @@
 import * as React from 'react';
+import * as _ from 'lodash';
+
+const CHARS = 20;
+const LINES = 20;
 
 export function App() {
-  return <h1>Hello world!</h1>;
+  const [text, setText] = React.useState('')
+
+  let lines = text.replace(/^\n+|\n+$/g, '').split('\n')
+  let pages = _.chunk(lines, LINES)
+
+  return (
+    <div className='app'>
+      <div className='control-menu'>
+        <textarea className='w-full rounded border-2 m-2' onChange={(event) => setText(event.target.value)}>{text}</textarea>
+      </div>
+      <div className='papers'>
+        { pages.map((page, index: number) => {
+          return <div key={index} className='w-full m-4 paper'>
+            <div className='text'>
+              { [...Array(LINES)].map((_, index: number) => {
+                let line = page[index] || ''
+                return <div key={index} className='line'>
+                  {line.split('').map((ch, index: number) => {
+                    if (ch == '\n') {
+                      return <br key={index} />
+                    } else {
+                      return <span key={index}>{ch}</span>
+                    }
+                  })}
+                </div>
+              })}
+            </div>
+            <div className='background'>
+              { [...Array(LINES)].map((_, index: number) => 
+                <div key={index} className='line'>
+                  { [...Array(CHARS)].map((_, index: number) => <span key={index}>　</span>)}
+                </div>
+              ) }
+            </div>
+            <div className='footer'>（{CHARS}✕{LINES} {CHARS * LINES}字）</div>
+          </div>
+        })}
+      </div>
+    </div>
+  )
 }

835cf3c1099c9687f31d4ff2d1906a104d2d986e
hogelog <konbu.komuro@gmail.com>
2022-08-30 23:28:04 +0900

    Implement auto newline logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,54 @@ import * as _ from 'lodash';
 const CHARS = 20;
 const LINES = 20;
 
+const KINSOKU_START_NG = [
+  '、', '。',
+  '，', '．',
+  '）', '」', '｝',
+  '・', 'ー', '―', '−',
+  '！', '？',
+  '々',
+  'ぁ', 'ぃ', 'ぅ', 'ぇ', 'ぉ',
+  'ァ', 'ィ', 'ゥ', 'ェ', 'ォ',
+  'っ', 'ゃ', 'ゅ', 'ょ',
+  'ッ', 'ャ', 'ュ', 'ョ',
+]
+const KINSOKU_END_NG = [
+  '（', '「', '｛',
+]
+function kumihan(text: string) {
+  let characters = text.split('')
+  let line = []
+  let lines = [line]
+  for (let i=0; i<characters.length; ++i) {
+    let ch = characters[i]
+    if (ch == '\n') {
+        lines.push(line = [])
+    } else if (line.length == 0) {
+      if (KINSOKU_START_NG.includes(ch) && lines.length > 1) {
+        lines[lines.length - 2].push(ch)
+      } else {
+        line.push(ch)
+      }
+    } else if (line.length < CHARS - 1) {
+      line.push(ch)
+    } else if (line.length == CHARS - 1) {
+      if (KINSOKU_END_NG.includes(ch)) {
+        lines.push(line = [])
+        line.push(ch)
+      } else {
+        line.push(ch)
+        lines.push(line = [])
+      }
+    }
+  }
+  return _.chunk(lines, LINES)
+}
+
 export function App() {
   const [text, setText] = React.useState('')
 
-  let lines = text.replace(/^\n+|\n+$/g, '').split('\n')
-  let pages = _.chunk(lines, LINES)
+  let pages = kumihan(text)
 
   return (
     <div className='app'>
@@ -18,15 +61,15 @@ export function App() {
           document.title = value.split('\n')[0]
           setText(value)
         }}>{text}</textarea>
+        <p>Confirmed only Google Chrome 104 on macOS 12.5.1.</p>
       </div>
       <div className='papers'>
         { pages.map((page, index: number) => {
           return <div key={index} className='w-full m-4 paper'>
             <div className='text'>
-              { [...Array(LINES)].map((_, index: number) => {
-                let line = page[index] || ''
+              { page.map((line, index: number) => {
                 return <div key={index} className='line'>
-                  {line.split('').map((ch, index: number) => {
+                  { line.map((ch, index: number) => {
                     if (ch == '\n') {
                       return <br key={index} />
                     } else {
